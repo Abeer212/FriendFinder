38278166c1bcc4e82ef0c3f115070e210b0f1c8a
AJ <abir.hossain212@gmail.com>
2019-07-08 22:03:44 -0400

    Final app

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+var friends = [
+  {
+    name: "Homer",
+    photo:
+      "https://i.pinimg.com/236x/b4/0f/f7/b40ff76afc6edcbf69b0190f1004ad80--simpsons-cartoon-simpsons-characters.jpg",
+    scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+  },
+  {
+    name: "Marge",
+    photo:
+      "https://i.pinimg.com/originals/59/b8/bb/59b8bbbe5236636220d33f5871858b59.jpg",
+    scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+  },
+  {
+    name: "Lisa",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/e/ec/Lisa_Simpson.png/220px-Lisa_Simpson.png",
+    scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
+  },
+  {
+    name: "Bart",
+    photo:
+      "https://vignette.wikia.nocookie.net/simpsons/images/6/65/Bart_Simpson.png/revision/latest/scale-to-width-down/160?cb=20190409004756",
+    scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
+  },
+  {
+    name: "Apu",
+    photo:
+      "https://spectatorau.imgix.net/content/uploads/2018/04/a.png?auto=compress,enhance,format&w=820&h=542",
+    scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
+  },
+  {
+    name: "Milhouse",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/1/11/Milhouse_Van_Houten.png/220px-Milhouse_Van_Houten.png",
+    scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
+  }
+];
+
+module.exports = friends;
